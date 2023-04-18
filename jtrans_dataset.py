9a993905ad58e12ad6f15f22524eb3abb7517933
liuli <2523008742@qq.com>
2023-04-18 13:19:57 +0000

    data_read

--- jtrans_dataset.py
+++ jtrans_dataset.py
@@ -2,9 +2,22 @@ import torch
 from text_process import tokenize, Vocab
 import random
 import os
+import re
 
 
-def _get_next_sentence(sentence, next_sentence, paragraphs):
+def read_blocks(data_dir):
+    with open(data_dir, 'r') as f:
+        p_lines = f.readlines()
+    res_lines = []
+    for line in p_lines:
+        if line[:4] == '[ph]':
+            continue
+        res_lines.append(line[:-1].split('\t'))
+
+    return res_lines
+
+
+def get_next_sentence(sentence, next_sentence, paragraphs):
     if random.random() < 0.5:
         is_next = True
     else:
@@ -27,19 +40,26 @@ def get_tokens_and_segments(tokens_a, tokens_b=None):
     return tokens, segments
 
 
-def _get_nsp_data_from_paragraph(paragraph, paragraphs, vocab, max_len):
+def _get_nsp_data_from_paragraph(block, blocks, vocab, max_len):
     nsp_data_from_paragraph = []
-    for i in range(len(paragraph) - 1):
-        tokens_a, tokens_b, is_next = _get_next_sentence(paragraph[i], paragraph[i + 1], paragraphs)
-        # 考虑1个'<cls>'词元和2个'<sep>'词元
+    for i in range(len(block) - 1):
+        tokens_a, tokens_b, is_next = get_next_sentence(block[i], block[i + 1], blocks)
         if len(tokens_a) + len(tokens_b) + 3 > max_len:
             continue
         tokens, segments = get_tokens_and_segments(tokens_a, tokens_b)
         nsp_data_from_paragraph.append((tokens, segments, is_next))
+        match_jump_id = re.match(r'^JUMP_ADDR_([0-9]*)$', block[i][-1], re.M | re.I)
+        if match_jump_id:
+            jump_addr = int(match_jump_id.group(1))
+            tokens_a, tokens_b, is_next = get_next_sentence(block[i], block[jump_addr], blocks)
+            if len(tokens_a) + len(tokens_b) + 3 > max_len:
+                continue
+            tokens, segments = get_tokens_and_segments(tokens_a, tokens_b)
+            nsp_data_from_paragraph.append((tokens, segments, is_next))
     return nsp_data_from_paragraph
 
 
-def _replace_mlm_tokens(tokens, candidate_pred_positions, num_mlm_preds, vocab):
+def replace_mlm_tokens(tokens, candidate_pred_positions, num_mlm_preds, vocab):
     # 为遮蔽语言模型的输入创建新的词元副本，其中输入可能包含替换的“<mask>”或随机词元
     mlm_input_tokens = [token for token in tokens]
     pred_positions_and_labels = []
@@ -74,14 +94,14 @@ def _get_mlm_data_from_tokens(tokens, vocab):
         candidate_pred_positions.append(i)
     # 遮蔽语言模型任务中预测15%的随机词元
     num_mlm_preds = max(1, round(len(tokens) * 0.15))
-    mlm_input_tokens, pred_positions_and_labels = _replace_mlm_tokens(tokens, candidate_pred_positions, num_mlm_preds, vocab)
+    mlm_input_tokens, pred_positions_and_labels = replace_mlm_tokens(tokens, candidate_pred_positions, num_mlm_preds, vocab)
     pred_positions_and_labels = sorted(pred_positions_and_labels, key=lambda x: x[0])
     pred_positions = [v[0] for v in pred_positions_and_labels]
     mlm_pred_labels = [v[1] for v in pred_positions_and_labels]
     return vocab[mlm_input_tokens], pred_positions, vocab[mlm_pred_labels]
 
 
-def _pad_bert_inputs(examples, max_len, vocab):
+def pad_bert_inputs(examples, max_len, vocab):
     max_num_mlm_preds = round(max_len * 0.15)
     all_token_ids, all_segments, valid_lens,  = [], [], []
     all_pred_positions, all_mlm_weights, all_mlm_labels = [], [], []
@@ -99,17 +119,17 @@ def _pad_bert_inputs(examples, max_len, vocab):
     return (all_token_ids, all_segments, valid_lens, all_pred_positions, all_mlm_weights, all_mlm_labels, nsp_labels)
 
 
-class _WikiTextDataset(torch.utils.data.Dataset):
-    def __init__(self, paragraphs, max_len):
+class _JTransTextDataset(torch.utils.data.Dataset):
+    def __init__(self, blocks, max_len):
         # 输入paragraphs[i]是代表段落的句子字符串列表；
         # 而输出paragraphs[i]是代表段落的句子列表，其中每个句子都是词元列表
-        paragraphs = [tokenize(paragraph, token='word') for paragraph in paragraphs]
-        sentences = [sentence for paragraph in paragraphs for sentence in paragraph]
-        self.vocab = Vocab(sentences, min_freq=5, reserved_tokens=['<pad>', '<mask>', '<cls>', '<sep>'])
+        blocks = [tokenize(block, token='word') for block in blocks]
+        instructions = [instruction for block in blocks for instruction in block]
+        self.vocab = Vocab(instructions, min_freq=5, reserved_tokens=['<pad>', '<mask>', '<cls>', '<sep>'])
         # 获取下一句子预测任务的数据
         examples = []
-        for paragraph in paragraphs:
-            examples.extend(_get_nsp_data_from_paragraph(paragraph, paragraphs, self.vocab, max_len))
+        for block in blocks:
+            examples.extend(_get_nsp_data_from_paragraph(block, blocks, self.vocab, max_len))
         # 获取遮蔽语言模型任务的数据
         examples = [(_get_mlm_data_from_tokens(tokens, self.vocab) + (segments, is_next)) for tokens, segments, is_next in examples]
         # 填充输入
@@ -119,7 +139,7 @@ class _WikiTextDataset(torch.utils.data.Dataset):
          self.all_pred_positions,
          self.all_mlm_weights,
          self.all_mlm_labels,
-         self.nsp_labels) = _pad_bert_inputs(examples, max_len, self.vocab)
+         self.nsp_labels) = pad_bert_inputs(examples, max_len, self.vocab)
 
     def __getitem__(self, idx):
         return (self.all_token_ids[idx],
@@ -134,48 +154,14 @@ class _WikiTextDataset(torch.utils.data.Dataset):
         return len(self.all_token_ids)
 
 
-def _read_wiki(data_dir):
-    file_name = os.path.join(data_dir, 'wiki.train.tokens')
-    with open(file_name, 'r') as f:
-        lines = f.readlines()
-    # 大写字母转换为小写字母
-    paragraphs = [line.strip().lower().split(' . ') for line in lines if len(line.split(' . ')) >= 2]
-    random.shuffle(paragraphs)
-    return paragraphs
-
-
-def _read_jTrans(data_dir):
-    file_name = os.path.join(data_dir, 'paragraphs.txt')
-    with open(file_name, 'r') as f:
-        lines = f.readlines()
-    # 大写字母转换为小写字母
-    paragraphs = [line.strip().lower().split(' . ') for line in lines if len(line.split(' . ')) >= 2]
-    random.shuffle(paragraphs)
-    return paragraphs
-
-
-def load_data_wiki(data_dir, batch_size, max_len, num_workers):
-    """加载WikiText-2数据集"""
-    paragraphs = _read_wiki(data_dir)
-    train_set = _WikiTextDataset(paragraphs, max_len)
-    train_iter = torch.utils.data.DataLoader(train_set, batch_size, shuffle=True, num_workers=num_workers)
-    return train_iter, train_set.vocab
-
-def load_jTrans_data(data_dir, batch_size, max_len, num_workers):
-    paragraphs = _read_jTrans(data_dir)
-    train_set = _WikiTextDataset(paragraphs, max_len)
+def load_data_jtrans(data_dir, batch_size, max_len, num_workers):
+    blocks = read_blocks(data_dir)
+    train_set = _JTransTextDataset(blocks, max_len)
     train_iter = torch.utils.data.DataLoader(train_set, batch_size, shuffle=True, num_workers=num_workers)
     return train_iter, train_set.vocab
 
 
 if __name__ == '__main__':
     batch_size, max_len = 512, 64
-    data_path = './data'
-    train_iter, vocab = load_data_wiki(data_path, batch_size, max_len, 4)
-
-    for (tokens_X, segments_X, valid_lens_x, pred_positions_X, mlm_weights_X,
-         mlm_Y, nsp_y) in train_iter:
-        print(tokens_X.shape, segments_X.shape, valid_lens_x.shape,
-              pred_positions_X.shape, mlm_weights_X.shape, mlm_Y.shape,
-              nsp_y.shape)
-        break
+    data_path = './data/paragraphs.txt'
+    load_data_jtrans(data_path, batch_size, max_len, 4)
